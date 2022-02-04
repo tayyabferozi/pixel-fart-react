3a22fa0e9770c00b68161f46d336e46b2cb9b470
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-02-04 19:22:17 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div class="empty"></div>
+        <div class="logo-container">
+          <img class="logo" src="./assets/img/logo.jpeg" alt="" />
+        </div>
+
+        <div class="social-container">
+          <ul class="social-icons">
+            <li>
+              <a href="https://discord.gg/QXcgRTcbrj">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/pixelfartnft/">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/PixelFartNFT">
+                <i class="fab fa-discord"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </header>
-    </div>
+      <div class="frame-container">
+        <div class="frame">
+          <img class="nft-vector" src="./assets/img/nft.jpeg" alt="" />
+        </div>
+      </div>
+
+      <div class="qr-container">
+        <img class="qr" src="./assets/img/qr.jpeg" alt="" />
+      </div>
+
+      <div class="fancy-text-container">
+        <div class="text-box">
+          <h1>SCAN TO ANIMATE</h1>
+          <h1>SCAN TO ANIMATE</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
