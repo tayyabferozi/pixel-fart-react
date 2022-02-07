200c5972192d42698d3851426a2eacdbd1020de8
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-02-08 01:45:13 +0500

    fixes size of img

--- src/App.js
+++ src/App.js
@@ -2,43 +2,43 @@ function App() {
   return (
     <>
       <header>
-        <div class="empty"></div>
-        <div class="logo-container">
-          <img class="logo" src="./assets/img/logo.jpeg" alt="" />
+        <div className="empty"></div>
+        <div className="logo-container">
+          <img className="logo" src="./assets/img/logo.jpeg" alt="" />
         </div>
 
-        <div class="social-container">
-          <ul class="social-icons">
+        <div className="social-container">
+          <ul className="social-icons">
             <li>
               <a href="https://discord.gg/QXcgRTcbrj">
-                <i class="fab fa-instagram"></i>
+                <i className="fab fa-instagram"></i>
               </a>
             </li>
             <li>
               <a href="https://www.instagram.com/pixelfartnft/">
-                <i class="fab fa-twitter"></i>
+                <i className="fab fa-twitter"></i>
               </a>
             </li>
             <li>
               <a href="https://twitter.com/PixelFartNFT">
-                <i class="fab fa-discord"></i>
+                <i className="fab fa-discord"></i>
               </a>
             </li>
           </ul>
         </div>
       </header>
-      <div class="frame-container">
-        <div class="frame">
-          <img class="nft-vector" src="./assets/img/nft.jpeg" alt="" />
+      <div className="frame-container">
+        <div className="frame">
+          <img className="nft-vector" src="./assets/img/nft.jpeg" alt="" />
         </div>
       </div>
 
-      <div class="qr-container">
-        <img class="qr" src="./assets/img/qr.jpeg" alt="" />
+      <div className="qr-container">
+        <img className="qr" src="./assets/img/qr.jpeg" alt="" />
       </div>
 
-      <div class="fancy-text-container">
-        <div class="text-box">
+      <div className="fancy-text-container">
+        <div className="text-box">
           <h1>SCAN TO ANIMATE</h1>
           <h1>SCAN TO ANIMATE</h1>
         </div>
